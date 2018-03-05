950efa96693e198f234f43c57ebf9e3551385c04
Bluewind12 <zero18030@gmail.com>
2018-03-05 19:02:45 +0900

    kari

--- CrownCheck.java
+++ CrownCheck.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.GridPane;
@@ -17,46 +18,47 @@ import javafx.stage.Stage;
  * CrownCheck
  */
 public class CrownCheck extends Application {
+    //モンスター名 
+    Text[] mTexts = { new Text("ドスジャグラス"), new Text("クルルヤック"), new Text("プケプケ"), new Text("ボルボロス"), new Text("ジュラトドス"),
+            new Text("トビカガチ"), new Text("アンジャナフ"), new Text("リオレイア"), new Text("ツィツィヤック"), new Text("パオウルムー"),
+            new Text("ドスギルオス"), new Text("ラドバルキン"), new Text("レイギエナ"), new Text("オドガロン"), new Text("リオレウス"),
+            new Text("ディアブロス"), new Text("キリン"), new Text("ゾラ・マグダラオス"), new Text("ドドガマル"), new Text("リオレイア亜種"),
+            new Text("バゼルギウス"), new Text("ヴォルガノス"), new Text("ウラガンキン"), new Text("リオレウス亜種"), new Text("ディアブロス亜種"),
+            new Text("ネルギガンテ"), new Text("テオ・テスカトル"), new Text("クシャルダオラ"), new Text("ヴァルハザク"), new Text("ゼノ・ジーヴァ") };
 
-    private String fileName = "MHWText.txt";
-    //モンスター名
-    String[] monsterName = new String[30];
-    Text[] mTexts = new Text[30];
-
-    //金冠Check
-    int[] bigCrown = new int[30];
-    int[] smallCrown = new int[30];
-
-    CheckBox[] bigCrownBoxs = new CheckBox[30];
-    CheckBox[] smallCrownBoxs = new CheckBox[30];
+    CheckBox[] bigCrownBoxs = { new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"),
+            new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"),
+            new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"),
+            new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"),
+            new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"),
+            new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"), new CheckBox("最大"),
+            new CheckBox("最大"), };
+    CheckBox[] smallCrownBoxs = { new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"),
+            new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"),
+            new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"),
+            new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"),
+            new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"),
+            new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"), new CheckBox("最小"),
+            new CheckBox("最小"), };
 
     public static void main(String[] args) {
         launch();
     }
 
     public void start(Stage stage) {
-        try {
-            File file = new File(fileName);
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            for (int i = 0; i < monsterName.length; i++) {
-                monsterName[i] = br.readLine();
-                bigCrown[i] = Integer.parseInt(br.readLine());
-                smallCrown[i] = Integer.parseInt(br.readLine());
-            }
-            br.close();
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        Button reloadButton = new Button("更新");
         GridPane mGridPane = new GridPane();
-        for (int i = 0; i < monsterName.length; i++) {
-            mGridPane.add(mTexts[i], 0, i);
-            mGridPane.add(bigCrownBoxs[i], 1, i);
-            mGridPane.add(smallCrownBoxs[i], 2, i);
+        mGridPane.setHgap(10);
+        mGridPane.setVgap(10);
+        for (int i = 0; i < 30; i++) {
+            GridPane.setConstraints(mTexts[i], 0, i);
+            GridPane.setConstraints(bigCrownBoxs[i], 1, i);
+            GridPane.setConstraints(smallCrownBoxs[i], 2, i);
         }
-        mGridPane.add(reloadButton, 0, 99);
+        for (int k = 0; k < 30; k++) {
+            mGridPane.getChildren().add(mTexts[k]);
+            mGridPane.getChildren().add(bigCrownBoxs[k]);
+            mGridPane.getChildren().add(smallCrownBoxs[k]);
+        }
         Scene topScene = new Scene(mGridPane);
         stage.setScene(topScene);
         stage.show();
